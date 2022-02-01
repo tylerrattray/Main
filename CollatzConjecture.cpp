b0a9c7d5dac3ea7d71a56f80540c79c3754a3350
Tyler Valentino Rattray <98286033+tylerrattray@users.noreply.github.com>
2022-02-01 21:09:42 +0000

    Update CollatzConjecture.cpp

--- CollatzConjecture.cpp
+++ CollatzConjecture.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-unsigned int n; unsigned int s; unsigned int c = 0; unsigned int l; unsigned int p;
+int num; int end; int count; int pnum;
 
 int main() {
-	std::cout << "Please enter a starting number: ";std::cin >> n;
-	std::cout << "Please enter a ending number: ";std::cin >> s;
-	p = n;
-	l = (s+1) - n;
-	while (l >1) {
-		if (n % 2 == 0) {
-			n = n / 2;c+= 1;
+	std::cout << "Please enter a starting number: "; std::cin >> num;
+	std::cout << "Please enter a ending number: "; std::cin >> end;
+	end += 1;
+	if (num == 1) { num++; count = 0;}
+	else { count = 1; }
+	for (int i = end-num; i>0; i--) {
+		pnum = num;
+		do {
+			if (num % 2 == 0) {num = num / 2; count += 1;}
+			else {num = 3 * num + 1; count += 1;}
 		}
-		else if (n == 1) {
-			std::cout << "Number = " << p << " Steps = " << c << std::endl;
-			n = p += 1;
-			l -= 1;
-			c = 1;
-		}
-		else{
-			n = 3 * n + 1;c+= 1;
-		}
+		while (num > 1);
+		std::cout << "Number = " << pnum << " Steps = " << count <<"\n";
+		num = pnum + 1;
+		count = 1;
 	}
+	return 0;
 }
