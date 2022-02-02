9c0c8621780bf71b0ce2f40c66c36885d5e7d84a
Tyler Valentino Rattray <98286033+tylerrattray@users.noreply.github.com>
2022-02-02 12:24:03 +0000

    Update CollatzConjecture.cpp

--- CollatzConjecture.cpp
+++ CollatzConjecture.cpp
@@ -4,17 +4,15 @@ int num; int end; int count; int pnum;
 int main() {
 	std::cout << "Please enter a starting number: "; std::cin >> num;
 	std::cout << "Please enter a ending number: "; std::cin >> end;
-	end += 1;
-	if (num == 1) { num++; count = 0;}
+	if (num == 1) { num++; count = 0; }
 	else { count = 1; }
-	for (int i = end-num; i>0; i--) {
+	for (int i = (end+1)- num; i > 0; i--) {
 		pnum = num;
 		do {
-			if (num % 2 == 0) {num = num / 2; count += 1;}
-			else {num = 3 * num + 1; count += 1;}
-		}
-		while (num > 1);
-		std::cout << "Number = " << pnum << " Steps = " << count <<"\n";
+			if (num % 2 == 0) { num = num / 2; count += 1; }
+			else { num = 3 * num + 1; count += 1; }
+		} 		while (num > 1);
+		std::cout << "Number = " << pnum << " Steps = " << count << "\n";
 		num = pnum + 1;
 		count = 1;
 	}
